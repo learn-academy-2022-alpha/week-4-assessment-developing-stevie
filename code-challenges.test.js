ae2442e707972541e301810e9bf741240151deb3
developing-stevie <mitchellevie@icloud.com>
2022-03-23 14:27:04 -0400

    corrected assessment week 4

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,6 +1,6 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
-const { concat } = require("lodash")
+// const { concat } = require("lodash")
 
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
@@ -19,26 +19,53 @@ const { concat } = require("lodash")
 
 
 
-// // a describe method that lists the name of the function OR naming of the particular test.
-// describe("newArr", () => {
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("newArr", () => {
 
-//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
-//     it("that takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
   
-//       const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-// const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+      const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
 
-//       expect(newArr(colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
-//       expect(newArr(colors2)).toEqual(expect.arrayContaining(["indigo", "periwinkle", "ochre", "aquamarine", "saffron"]))
-//     })
-//   })
+      expect(newArr(colors1)).toEqual(expect.arrayContaining(["blue", "green", "yellow", "pink"]))
+      expect(newArr(colors2)).toEqual(expect.arrayContaining(["indigo", "periwinkle", "ochre", "aquamarine", "saffron"]))
+    })
+  })
 
 
 // b) Create the function that makes the test pass.
+
+
+// let array equal (colors1.shift())
+// shuffle function array
+// sort array
+// random number 
+// return shuffle(array)
+
+
+
+
+  const newArr = (array) => {
+    array.shift()
+ return array.sort((a,b) => Math.random() - 0.5);
+}; 
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
+  // FAIL  ./code-challenges.test.js
+  // array
+
+
+  // Test Suites: 1 failed, 1 total
+  // Tests:       1 failed, 1 total
+
 
 // create a function shuff that takes in an array as a peram
 // use shift to remove index
@@ -65,31 +92,6 @@ const { concat } = require("lodash")
 // FAIL  ./code-challenges.test.js
 // fleDraw
 
-// let array equal (colors1.shift())
-// shuffle function array
-// sort array
-// random number 
-// return shuffle(array)
-
-
-
-
-//   const newArr = (array) => {
-//     array.shift()
-//  return array.sort((a,b) => Math.random() - 0.5);
-// }; 
-
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-
-
-  // FAIL  ./code-challenges.test.js
-  // array
-
-
-  // Test Suites: 1 failed, 1 total
-  // Tests:       1 failed, 1 total
-
 
   // FAIL  ./code-challenges.test.js
   // fleDraw
@@ -103,23 +105,23 @@ const { concat } = require("lodash")
 
 
 
-// // a describe method that lists the name of the function OR naming of the particular test.
-// describe("lessThan", () => {
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("lessThan", () => {
 
-//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
-//     it("that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
   
-//       const nums1 = [3, 56, 90, -8, 0, 23, 6]
-//       // Expected output: [-8, 90]
-//       const nums2 = [109, 5, 9, 67, 8, 24]
-//       // Expected output: [5, 109]
+      const nums1 = [3, 56, 90, -8, 0, 23, 6]
+      // Expected output: [-8, 90]
+      const nums2 = [109, 5, 9, 67, 8, 24]
+      // Expected output: [5, 109]
       
 
-//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
-//       expect(lessThan(nums1)).toEqual([-8, 90])
-//       expect(lessThan(nums2)).toEqual([5, 109])
-//     })
-//   })
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(lessThan(nums1)).toEqual([-8, 90])
+      expect(lessThan(nums2)).toEqual([5, 109])
+    })
+  })
 
 
 
@@ -135,9 +137,9 @@ const { concat } = require("lodash")
 
 
 
-//   const lessThan = (array) => {
-//  return minlessThan = [Math.min(...array), Math.max(...array)]
-//   }
+  const lessThan = (array) => {
+ return minlessThan = [Math.min(...array), Math.max(...array)]
+  }
   
 
   // Test Suites: 1 passed, 1 total
@@ -165,16 +167,17 @@ describe("bothArr", () => {
     it("that takes in two arrays as arguments and returns one array with no duplicate values", () => {
   
       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
-      expect(bothArr()).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+      expect(bothArr(testArray1,testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
     })
   })
 
 
 // b) Create the function that makes the test pass.
 
-// let bothArr = [].concat(testArray2,testArray1
-// bothArr.filter ((item,index,self) 
-// self.indexOf(item)== index);
+//make const that combines both arrays
+//and then returns a new set of the arrays
+// without duplicate numbers names bothArr
+// could not get the test to pass on either code
 
 
 
@@ -202,3 +205,9 @@ return bothArr = [...new Set]([...testArray1, ...testArray2])
 
 // Test Suites: 1 failed, 1 total
 // Tests:       1 failed, 1 total
+
+//----------old code but psudo
+
+// let bothArr = [].concat(testArray2,testArray1
+// bothArr.filter ((item,index,self) 
+// self.indexOf(item)== index);
